735f78a3f0562ec42a80137379a44f26f973c5a9
lazycoder <alazycoder101@gmail.com>
2021-10-02 01:49:37 +0100

    friendlify id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  extend FriendlyId
+  friendly_id :friendify, use: :slugged
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
